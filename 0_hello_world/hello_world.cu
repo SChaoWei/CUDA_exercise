8c7054c4c5c3a15da5c10692c62b69c67ed6306d
scw <1415686499@qq.com>
2024-04-18 08:51:03 +0800

    第一次练习

--- 0_hello_world/hello_world.cu
+++ 0_hello_world/hello_world.cu
@@ -1,12 +1,12 @@
 #include<stdio.h>
 __global__ void hello_world(void)
 {
-  printf("GPU: Hello world!\n");
+  printf("hello GPU");
 }
-int main(int argc,char **argv)
+int main()
 {
-  printf("CPU: Hello world!\n");
-  hello_world<<<1,10>>>();
-  cudaDeviceReset();//if no this line ,it can not output hello world from gpu
+  printf("hello CPU");
+  hello_world<<<1,6>>>();
+  cudaDeviceReset();
   return 0;
 }
